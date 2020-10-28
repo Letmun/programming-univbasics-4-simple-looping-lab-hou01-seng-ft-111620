c19b2c457ced57669f3536fc1250d5af15263b54
Duncan Stark <letmunplaysgames@gmail.com>
2020-10-28 19:29:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,11 +15,4 @@
         count += 1
       end
     end
-    def return_string_array(array)
-  count = 0
-  while count < array.length do
-    array[count] = array[count].to_s
-    count += 1
-  end
-  array
-end
+   
