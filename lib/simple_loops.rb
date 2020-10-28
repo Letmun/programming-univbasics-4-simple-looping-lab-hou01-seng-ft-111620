a70d6ef29eac1a73a325d875e46e4e55e4f44b00
Duncan Stark <letmunplaysgames@gmail.com>
2020-10-28 17:30:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
 # Write your methods here
+      def return_string_array(array)
+      count = 0
+      new_array = []
+      while count < array.length do
+        new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+        count += 1
+      end
+      new_array
+    end
+        def loop_message_five_times(message)
+      count = 0
+      while count < 5 do
+        puts message
+        count += 1
+      end
+    end
+    def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
